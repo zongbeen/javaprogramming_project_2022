e77eed72e4f3f76e34a9c3b092fdff26771ec3ba
Zongbeen Han <95284271+zongbeen@users.noreply.github.com>
2022-04-01 23:15:25 +0900

    Add files via upload

--- LoginFrame.java
+++ LoginFrame.java
@@ -1,31 +1,42 @@
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 public class LoginFrame extends JFrame {
     LoginFrame() {
         setTitle("Login");
-
         setLayout(new BorderLayout(10,10));
         showNorth();
         showCenter();
-        //showSouth();
+        showSouth();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300,220);
+        setSize(1000,1000);
         setVisible(true);
     }
 
 
-    private void showNorth() {
+    public void showNorth() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JLabel label = new JLabel("JAVA POS");
+        Font font = new Font("D2Coding ligature", Font.BOLD,80);
+        label.setFont(font);
+        label.setForeground(Color.BLUE);
+        panel.add(label);
+        add(panel, BorderLayout.NORTH);
+
+    }
+    public void showCenter() {
         JPanel p1 = new JPanel();
         JPanel p2 = new JPanel();
         JPanel panel = new JPanel(new GridLayout(2,0));
 
-        JLabel l1 = new JLabel("아이디");
+        JLabel l1 = new JLabel(" 아이디 ");
         JLabel l2 = new JLabel("비밀번호");
 
         JTextField t1 = new JTextField(10);
         JTextField t2 = new JTextField(10);
-        t2.setEnabled(false);
+        t2.setEnabled(true);
 
         p1.add(l1);
         p1.add(t1);
@@ -34,15 +45,22 @@ public class LoginFrame extends JFrame {
         panel.add(p1);
         panel.add(p2);
 
-        add(panel, BorderLayout.NORTH);
+        add(panel, BorderLayout.CENTER);
+
     }
-    private void showCenter() {
+    public void showSouth() {
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         JButton login = new JButton("로그인");
-
+        login.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setVisible(false);
+                new OrderFrame();
+            }
+        });
         panel.add(login);
-        add(panel, BorderLayout.CENTER);
+        add(panel, BorderLayout.SOUTH);
 
     }
     public static void main(String[] args) {
---
+++ OrderFrame.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+public class OrderFrame extends JFrame {
+    JButton button1 = new JButton("1");
+    JButton button2 = new JButton("2");
+    JButton button3 = new JButton("3");
+    JButton button4 = new JButton("4");
+    JButton button5 = new JButton("5");
+    JButton button6 = new JButton("6");
+    JButton button7 = new JButton("7");
+    JButton button8 = new JButton("8");
+    OrderFrame() {
+        next();
+        setTitle("OrderFrame");
+        setSize(1000,1000);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        button1.setBounds(50,300,200,150);
+        button2.setBounds(287,300,200,150);
+        button3.setBounds(517,300,200,150);
+        button4.setBounds(747,300,200,150);
+        button5.setBounds(50,500,200,150);
+        button6.setBounds(287,500,200,150);
+        button7.setBounds(517,500,200,150);
+        button8.setBounds(747,500,200,150);
+        getContentPane().add(button1);
+        getContentPane().add(button2);
+        getContentPane().add(button3);
+        getContentPane().add(button4);
+        getContentPane().add(button5);
+        getContentPane().add(button6);
+        getContentPane().add(button7);
+        getContentPane().add(button8);
+        setVisible(true);
+    }
+    public void next(){
+        button1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setVisible(false);
+                new OrderFrame2();
+            }
+        });
+        button2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setVisible(false);
+                new OrderFrame2();
+            }
+        });
+    }
+}
